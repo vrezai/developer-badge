f071c73791f9a7e5b7e9fe4f34c071e18030bcf2
Cameron <76537694+vrezai@users.noreply.github.com>
2022-11-25 11:16:26 -0500

    Add files via upload

---
+++ Developer Badge/app.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+bot = discord.Bot()
+
+# Creating an event in the console for when the bot is online!
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+# A discord slash command. /ping: says "Pong!"
+
+
+@bot.slash_command(description="Says Pong!")
+async def ping(ctx):
+    await ctx.respond("Pong!")
+
+bot.run('')
